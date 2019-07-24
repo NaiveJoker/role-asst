ef7b618ddd495eaadcc85eeeaf90decfb5515d7f
NaiveJoker <13595230723@163.com>
2019-07-24 10:32:59 +0800

    新增加结果展示在预览图，修改页面布局

--- src/js/index.js
+++ src/js/index.js
@@ -12,8 +12,7 @@ $(document).ready(function() {
     // canvas转码
     if (dataUrl) {
       //将base64图片转为对象
-      var blob = dataURItoBlob(dataUrl);
-      console.log(typeof blob);
+      let blob = dataURItoBlob(dataUrl);
       formData.append("screenshot", blob);
       formData.append("filename", "role.png");
     } else {
@@ -44,16 +43,25 @@ $(document).ready(function() {
           let result = data.result.face_list;
           console.log(result);
           emptyLi();
+          let canvas = drawCanvas();
           for (let i = 0; i < result.length; i++) {
             let userId = result[i].user_list;
+            let faceTop = result[i].location.top;
+            let faceLeft = result[i].location.left;
             if (userId.length) {
               let user_id = `姓名： ${userId[0].user_id}`;
               let score = `可信度： ${userId[0].score}`;
               insertResult(user_id, score);
+              let faceName = userId[0].user_id;
+              drawFace(canvas, faceLeft, faceTop, faceName);
             } else {
               insertResult("角色未入库", "暂无评分");
+              let faceName = "未入库";
+              drawFace(canvas, faceLeft, faceTop, faceName);
             }
           }
+          dataUrl = canvas.toDataURL("image/jpg");
+          $("#preview")[0].src = dataUrl;
         }
       },
       cache: false,
@@ -63,6 +71,49 @@ $(document).ready(function() {
   });
   //点击截图后用canvas截图
   $("#screenshot").click(function() {
+    dataUrl = drawCanvas().toDataURL("image/jpg");
+    $("#preview")[0].src = dataUrl;
+  });
+  function insertResult(info1, info2) {
+    let resultList = $("#result-list");
+    let li = templateLi(info1, info2);
+    resultList.append(li);
+  }
+  function templateLi(info1, info2) {
+    let li = `
+    <li class="list-group-item result-li">
+      <p class="result-p">${info1}</p>
+      <p class="result-p">${info2}</p>
+    </li>
+    `;
+    return li;
+  }
+  function emptyLi() {
+    let resultList = $("#result-list");
+    return resultList.empty();
+  }
+  // function activeLi() {
+  //   let li = $("#result-list li:first");
+  //   return li.addClass("active");
+  // }
+  //将base64转图片对象
+  function dataURItoBlob(base64Data) {
+    let byteString = "";
+    if (base64Data.split(",")[0].indexOf("base64") >= 0)
+      byteString = atob(base64Data.split(",")[1]);
+    else byteString = unescape(base64Data.split(",")[1]);
+    let mimeString = base64Data
+      .split(",")[0]
+      .split(":")[1]
+      .split(";")[0];
+    let ia = new Uint8Array(byteString.length);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], { type: mimeString });
+  }
+  //绘制视频帧canvas
+  function drawCanvas() {
     let canvas = $("<canvas>")[0];
     let canvasCtx = canvas.getContext("2d");
     let video = $("video")[0];
@@ -86,44 +137,14 @@ $(document).ready(function() {
       imgWidth,
       imgHeight
     );
-    dataUrl = canvas.toDataURL("image/jpg");
-    img.src = dataUrl;
-  });
-  function insertResult(info1, info2) {
-    let resultList = $("#result-list");
-    let li = templateLi(info1, info2);
-    resultList.append(li);
+    return canvas;
   }
-  function templateLi(info1, info2) {
-    let li = `
-    <li class="list-group-item result-li">
-      <p class="result-p">${info1}</p>
-      <p class="result-p">${info2}</p>
-    </li>
-    `;
-    return li;
-  }
-  function emptyLi() {
-    let resultList = $("#result-list");
-    return resultList.empty();
-  }
-  function activeLi() {
-    let li = $("#result-list li:first");
-    return li.addClass("active");
-  }
-  function dataURItoBlob(base64Data) {
-    var byteString;
-    if (base64Data.split(",")[0].indexOf("base64") >= 0)
-      byteString = atob(base64Data.split(",")[1]);
-    else byteString = unescape(base64Data.split(",")[1]);
-    var mimeString = base64Data
-      .split(",")[0]
-      .split(":")[1]
-      .split(";")[0];
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ia], { type: mimeString });
+  //在预览图中标记角色名称
+  function drawFace(canvas, faceLeft, faceTop, faceName) {
+    let canvasCtx = canvas.getContext("2d");
+    canvasCtx.font = "30px '微软雅黑'";
+    canvasCtx.fillStyle = "yellow";
+    canvasCtx.fillText(faceName, faceLeft, faceTop);
+    return canvas;
   }
 });
